6ba0dd732f3d062f82f9b584bfe798053bc3198f
tmannix-rough <92363632+tmannix-rough@users.noreply.github.com>
2021-11-02 19:13:14 +0000

    Updated license summary + added new license
    Filled out all summary items in license summary. Added a third license to the table.

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -35,17 +35,40 @@ module.exports = {
                   "licenceName": "Glindenfold Lake",
                   "startDate": "1 January 2019",
                   "endDate": "31 December 2029",
-                  "licenceHolder": "Bottled Water Plc",
-                  "sourceOfSupply": "something",
+                  "licenceHolder": "Bottled Water Plc  <br> <a href=app/views/licences/licences-table.html>View contact details</a>",
+                  "sourceOfSupply": "Underground strata comprising of Mercia Mudstone at Anarea, Atown.",
+                  "purpose": "Spray irrigation",
+                  "periodOfAbstraction": "March to September",
+                  "pointsOfAbstraction": "At National Grid Reference SK 675124 <br> At National Grid Reference SK 675343 <br> <a href=app/views/licences/licences-table.html>View details of abstraction point</a>",
+                  "abstractionConditions": "Eel screening condition <br> Flow cessation condition <br> Chemical cessation condition <br> <a href=app/views/licences/licences-table.html>View details of abstraction conditions</a>",
+                  "abstractionAmounts": "90 cubic metres per hour <br> 250 cubic metres per day <br> 20,000 cubnic metres per year"
                 },
                 {
                   "licenceNumber": "AN/02/233/003/0034",
                   "licenceName": "Bourton river",
                   "startDate": "1 January 2017",
                   "endDate": "31 December 2025",
-                  "licenceHolder": "Bottled Water Plc",
-                  "sourceOfSupply": "something",
+                  "licenceHolder": "Big Smith Farms Ltd <br> <a href=app/views/licences/licences-table.html>View contact details</a>",
+                  "sourceOfSupply": "Underground strata comprising of Mercia Mudstone at Anarea, Atown.",
+                  "purpose": "Spray irrigation",
+                  "periodOfAbstraction": "March to September",
+                  "pointsOfAbstraction": "At National Grid Reference SK 675124 <br> At National Grid Reference SK 675343 <br> <a href=app/views/licences/licences-table.html>View details of abstraction point</a>",
+                  "abstractionConditions": "Eel screening condition <br> Flow cessation condition <br> Chemical cessation condition <br> <a href=app/views/licences/licences-table.html>View details of abstraction conditions</a>",
+                  "abstractionAmounts": "90 cubic metres per hour <br> 250 cubic metres per day <br> 20,000 cubnic metres per year"
                 },
+                {
+                  "licenceNumber": "AN/02/233/003/0093",
+                  "licenceName": "Honeydew Crescent",
+                  "startDate": "5 September 2020",
+                  "endDate": "15 September 2034",
+                  "licenceHolder": "D. B. Beecham-Smith <br> <a href=app/views/licences/licences-table.html>View contact details</a>",
+                  "sourceOfSupply": "Underground strata comprising of Mercia Mudstone at Anarea, Atown.",
+                  "purpose": "Spray irrigation",
+                  "periodOfAbstraction": "February to December",
+                  "pointsOfAbstraction": "At National Grid Reference SK 675124 <br> At National Grid Reference SK 675343 <br> <a href=app/views/licences/licences-table.html>View details of abstraction point</a>",
+                  "abstractionConditions": "Eel screening condition <br> Flow cessation condition <br> Chemical cessation condition <br> <a href=app/views/licences/licences-table.html>View details of abstraction conditions</a>",
+                  "abstractionAmounts": "90 cubic metres per hour <br> 250 cubic metres per day <br> 20,000 cubnic metres per year"
+                }
               ],
 
 
